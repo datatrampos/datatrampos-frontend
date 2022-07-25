246473c9518b20d1f833c5ecefd5ba504a0c065c
Luís Farias <luisgmfarias@gmail.com>
2022-07-25 02:54:53 -0300

    fix navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,21 @@
 import Logo from "../../assets/datatrampos-text.svg";
-import { Nav } from "./styles";
+import { Nav, ImageContainer, Container } from "./styles";
 
 function Navbar() {
   return (
-    <Nav>
-      <div>
-        <a href="/sobre">Sobre</a>
-        <a href="http://localhost:3000">
-          <img src={Logo} alt="datatrampos-logo-title" />
-        </a>
-        <a href="/empresas">Empresas</a>
-      </div>
-    </Nav>
+    <Container>
+      <ImageContainer>
+        <Nav>
+          <div>
+            <a href="/sobre">Sobre</a>
+            <a href="http://localhost:3000">
+              <img src={Logo} alt="datatrampos-logo-title" />
+            </a>
+            <a href="/empresas">Empresas</a>
+          </div>
+        </Nav>
+      </ImageContainer>
+    </Container>
   );
 }
 
--- src/components/Navbar/styles.jsx
+++ src/components/Navbar/styles.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { COLORS } from "../../styles/colors";
+import background from "../../assets/background-gradient.svg";
 
 export const Nav = styled.div`
   display: flex;
@@ -23,4 +24,16 @@ export const Nav = styled.div`
   img {
     width: 400px;
   }
+`;
+
+export const ImageContainer = styled.div`
+  background-image: url(${background});
+  background-position: center;
+  background-size: contain;
+  padding: 30px 0px 50px 0px;
+  filter: drop-shadow(0px 4px 30px rgba(0, 0, 0, 0.2));
+`;
+
+export const Container = styled.div`
+  /* background-color: ${COLORS.darkRed}; */
 `;

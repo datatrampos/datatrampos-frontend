f88c93af533eccbbb8238df509dc8ef0ab64eda8
Luís Farias <luisgmfarias@gmail.com>
2022-07-25 02:52:40 -0300

    fix and features at api clients

--- src/api/clients/CompanyClient.js
+++ src/api/clients/CompanyClient.js
@@ -1,17 +1,8 @@
 import { axiosInstance } from "../core/apiConfig";
 
-export const CompanyClient = () => {
-  async function getCompanies() {
-    try {
-      const res = await axiosInstance.get("companies/");
-      const data = res.data;
-      return data;
-    } catch (error) {
-      return console.log(error);
-    }
-  }
+async function fetchCompanies() {
+  const { data } = await axiosInstance.get(`companies/`);
+  return data;
+}
 
-  return {
-    getCompanies,
-  };
-};
+export default fetchCompanies;
--- src/api/clients/JobClient.js
+++ src/api/clients/JobClient.js
@@ -1,7 +1,10 @@
 import { axiosInstance } from "../core/apiConfig";
 
-async function fetchJobs(page) {
-  const { data } = await axiosInstance.get(`jobs/?page=${page}`);
+async function fetchJobs(page = 1, company = "", remote = "") {
+  console.log(remote);
+  const { data } = await axiosInstance.get(
+    `jobs/?page=${page}&company=${company}&remote=${remote}`
+  );
   return data;
 }
 

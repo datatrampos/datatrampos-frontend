ddfebfb7c15011406831066c0db43972ec1980e7
Luís Farias <luisgmfarias@gmail.com>
2022-08-02 02:25:52 -0300

    fix presentation image to big

--- src/sections/Presentation/styles.jsx
+++ src/sections/Presentation/styles.jsx
@@ -14,6 +14,11 @@ export const ImageContainer = styled.img`
   /* border-radius: 35px; */
 
   width: 510px;
+
+  @media (max-width: 900px) {
+    width: 90%;
+  }
+
   /* 
   height: 287px; */
   /* filter: drop-shadow(0px 4px 10px rgba(0, 0, 0, 0.2)); */

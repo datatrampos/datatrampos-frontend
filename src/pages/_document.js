446967d5e93e1b967c5e02d659eb2d865e5c1633
luisgmfarias <luisgmfarias@gmail.com>
2022-08-21 03:25:21 -0300

    add gtm tracking id

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,6 +32,23 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <link rel="shortcut-icon" href="/favicon.ico" />
           <link
             rel="apple-touch-icon"
